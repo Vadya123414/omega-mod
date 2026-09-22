6aa8c848fadc5276e64fad737f6aef82fa366ae0
Vadya123414 <vadim.borodin.91@mail.ru>
2026-09-23 02:21:04 +0500

    чёта насисикалил

--- common/defines/defines.lua
+++ common/defines/defines.lua
@@ -62,6 +62,7 @@ NDefines.NNavy.NAVAL_MINES_ACCIDENT_CRITICAL_HIT_CHANCES = 0;    -- шанс к�
 NDefines.NNavy.NAVAL_MINES_ACCIDENT_CRITICAL_HIT_DAMAGE_SCALE = 0;   -- урон крита мин
 NDefines.NNavy.NAVAL_MINES_ACCIDENT_STRENGTH_LOSS = 0;      -- урон мин по прочности
 NDefines.NNavy.NAVAL_MINES_ACCIDENT_ORG_LOSS_FACTOR = 0;	--урон мин по орге
+NDefines.NNavy.TRAINING_ORG = 0.9; --0.2;   -- максимальная организация при обучении
 
 --Производство
 NDefines.NProduction.CAPITAL_SHIP_MAX_NAV_FACTORIES_PER_LINE = 10;
